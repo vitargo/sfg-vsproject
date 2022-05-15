03c43118ef20e287e33e7bf5f48c388ff86d1ff3
viktoriia.redko <viktoriia.redko@ericsson.com>
2022-05-15 13:29:29 +0300

    Add base Spring project generated by the Spring Initializr

---
+++ src/main/java/guru/springframework/vsproject/VsprojectApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.vsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/vsproject/VsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.vsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
