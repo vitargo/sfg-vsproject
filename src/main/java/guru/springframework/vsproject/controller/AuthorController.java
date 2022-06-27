a41ff04a74ace8ac7f0df504adcbbd6024674af5
viktoriia.redko <viktoriia.redko@ericsson.com>
2022-06-27 12:20:19 +0300

    Add Author Controller

---
+++ src/main/java/guru/springframework/vsproject/controller/AuthorController.java
@@ -0,0 +1,24 @@
+package guru.springframework.vsproject.controller;
+
+import guru.springframework.vsproject.repository.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
+}
